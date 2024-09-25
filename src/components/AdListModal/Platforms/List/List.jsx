00a48459f697b272f0ca0e1cbf14993ca31802db
chopeyyuriy <chopeyyuriy@gmail.com>
2024-09-25 20:00:30 +0300

    fix ad list modal

--- src/components/AdListModal/Platforms/List/List.jsx
+++ src/components/AdListModal/Platforms/List/List.jsx
@@ -2,12 +2,28 @@ import styled from "styled-components";
 import { Card } from "./Card";
 import flombuIcon from "../../../../assets/images/flombu.png";
 import olxIcon from "../../../../assets/images/olx.png";
+import { useGetStatusAccountQuery } from "../../../../store/objects/objects.api";
 
 export const List = ({ data }) => {
+  const { data: accounts } = useGetStatusAccountQuery();
+
+  const handleGetOlxAccount = (id) =>
+    accounts?.accounts?.find((a) => a.data?.id?.toString() === id)?.data;
+
+  const handleGetOlxTitle = (data) =>
+    data?.name ?? data?.phone ?? data?.email ?? data?.id;
+
   return (
     <StyledList>
-      {data?.arr_adverst_object?.map(({ resource }, i) => (
-        <Card icon={resource === "olx" ? olxIcon : flombuIcon} title={""} />
+      {data?.arr_adverst_object?.map(({ resource, id_user_olx }, i) => (
+        <Card
+          icon={resource === "olx" ? olxIcon : flombuIcon}
+          title={
+            resource === "olx"
+              ? handleGetOlxTitle(handleGetOlxAccount(id_user_olx))
+              : ""
+          }
+        />
       ))}
     </StyledList>
   );
