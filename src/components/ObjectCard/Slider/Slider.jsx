2f56563b6cf2be3edf447b2b6f271f35cb14de55
Dima Naymenko <dimanaymenko2005@gmail.com>
2025-07-09 10:03:51 +0300

    if data.index_overbuying is 0 show nothing in slider on car/:id page

--- src/components/ObjectCard/Slider/Slider.jsx
+++ src/components/ObjectCard/Slider/Slider.jsx
@@ -274,12 +274,14 @@ export const Slider = ({ photos, data, showLike, isCarPage }) => {
             </div>
           )}
 
-          {isCarPage && (
+          {isCarPage && data.index_overbuying != 0 && (
             <div className="text-sm stats absolute left-0 bottom-[10px] z-10">
               <div
                 className={`${+data.index_overbuying >= 8 && "bg-red-400"} ${
                   +data.index_overbuying >= 6 && "bg-orange-400"
-                } items-center flex gap-2 pl-2 pr-1 py-0.5`}
+                } ${
+                  +data.index_overbuying <= 5 && "bg-gray-400"
+                }  items-center flex gap-2 pl-2 pr-1 py-0.5`}
               >
                 <img src={Star} alt="" />
                 <span>{data.index_overbuying}/10</span>
